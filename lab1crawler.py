f9e4dceb3dee23d0322586490fafac81fb48ed6c
Isaack Mwenda <mwendaisaac@gmail.com>
2017-10-05 09:50:42 +0300

    Add files via upload

---
+++ lab1crawler.py
@@ -0,0 +1,13 @@
+import urllib.request
+import re
+
+#connecting to strathmore URL
+with urllib.request.urlopen('http://strathmore.edu/') as response:
+
+#reading html code  and converting it from byte to string
+   strathdata = response.read().decode('utf-8')
+
+#Getting html likns using re.findall    
+   strathlinks = re.findall('"((http)s?://.*?)"', strathdata)
+   for links in strathlinks:
+       print(links)
