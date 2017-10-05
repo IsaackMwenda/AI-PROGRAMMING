1aad90bea3c1ad3f576dd4614b53210bdf6d43be
Isaack Mwenda <mwendaisaac@gmail.com>
2017-10-05 09:55:57 +0300

    Update lab1crawler.py

--- lab1crawler.py
+++ lab1crawler.py
@@ -7,7 +7,7 @@ with urllib.request.urlopen('http://strathmore.edu/') as response:
 #reading html code  and converting it from byte to string
    strathdata = response.read().decode('utf-8')
 
-#Getting html likns using re.findall    
+#Getting html links using re.findall    
    strathlinks = re.findall('"((http)s?://.*?)"', strathdata)
    for links in strathlinks:
        print(links)
